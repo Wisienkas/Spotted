30e224f232cd14de8a75b1269216169cc9332011
Peter <petypetyy@gmail.com>
2016-04-09 17:03:12 +0200

    Signup redirect Signup functionality

--- app/src/main/java/io/mlh/spotted/spotted/Main_Activity.java
+++ app/src/main/java/io/mlh/spotted/spotted/Main_Activity.java
@@ -1,19 +1,19 @@
 package io.mlh.spotted.spotted;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-import com.parse.Parse;
-
 import io.mlh.spotted.spotted.Activities.Listeners.LoginListener;
-import io.mlh.spotted.spotted.Activities.Listeners.SignupListener;
 
 public class Main_Activity extends AppCompatActivity {
 
+
+    private EditText usernameField;
     private EditText passwordField;
-    private EditText usernameField; // Email used here
     private Button loginButton;
     private Button signupButton;
 
@@ -22,15 +22,23 @@ public class Main_Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        this.usernameField = (EditText) findViewById(R.id.username_input);
-        this.passwordField = (EditText) findViewById(R.id.password_input);
         this.loginButton = (Button) findViewById(R.id.login_button);
-        this.signupButton = (Button) findViewById(R.id.signup_button);
+        this.signupButton = (Button) findViewById(R.id.signup_redirect);
+        this.usernameField = (EditText)findViewById(R.id.login_name) ;
+        this.passwordField = (EditText)findViewById(R.id.login_password) ;
 
-        // Adding Signup
+
         this.loginButton.setOnClickListener(
                 new LoginListener(this.usernameField, this.passwordField, getApplicationContext()));
-        this.signupButton.setOnClickListener(
-                new SignupListener(this.usernameField, this.passwordField, getApplicationContext()));
+        this.signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Main_Activity.this, Signup_Activity.class);
+                //intent.putExtra("username", emailField.getText().toString());
+                startActivity(intent);
+            }
+        });
+        // Redirect to Signup activity
+        //this.signupRedirect.setOnClickListener();
     }
 }
